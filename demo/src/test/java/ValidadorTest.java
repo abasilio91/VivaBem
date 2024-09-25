66966e7c1021e75f3a37d19f92863399ae89f9d5
abasilio91 <adambasilio211@gmail.com>
2024-09-25 19:04:10 -0300

    refatorando codigo e criando a classe Validador

--- demo/src/test/java/ValidadorTest.java
+++ demo/src/test/java/ValidadorTest.java
@@ -4,22 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import br.com.vivabem.Pessoa;
+import br.com.vivabem.Validador;
 
-public class PessoaTest {
-    private Pessoa pessoa;
-    private String cpf = "12352192757";
-    private String nome = "joão silva";
-    private String dt_nascimento = "26/10/1989";
+public class ValidadorTest {
+    private String cpf;
 
     @BeforeEach
-    public void criaPessoa() throws Exception {
-        pessoa = new Pessoa(nome, cpf, dt_nascimento);
+    public void criCPF() {
+        cpf = "11144477735";
     }
-
+    
     @Test
     public void deveriaValidarOCPF() throws Exception {
-        boolean actual = Pessoa.validaCPF(cpf);
+        boolean actual = Validador.validaCPF(cpf);
         boolean expected = true;
         assertEquals(actual, expected);
     }
@@ -28,7 +25,7 @@ public class PessoaTest {
     public void deveriaFalharNaValidacaoDoCPFPorCausaDosDigitosVerificadores() {
         IllegalArgumentException thrown = assertThrows(
             IllegalArgumentException.class,
-            () -> Pessoa.validaCPF("11144477708")
+            () -> Validador.validaCPF("11144477708")
         );
 
         assertEquals("CPF inválido. Verifique os digitos.", thrown.getMessage());
@@ -38,7 +35,7 @@ public class PessoaTest {
     public void deveriaFalharNaValidacaoDoCPFPorTamanhoCPF() {
         Exception thrown = assertThrows(
             Exception.class,
-            () -> Pessoa.validaCPF("111444777")
+            () -> Validador.validaCPF("111444777")
         );
 
         assertEquals("CPF inválido. Era esperado 11 digitos.", thrown.getMessage());
