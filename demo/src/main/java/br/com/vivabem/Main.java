4c380f81e45324f03cb8a06f06df2e6bd5ef899d
abasilio91 <adambasilio211@gmail.com>
2024-09-30 20:27:26 -0300

    interface do usuario criada minimamente para testes'

--- demo/src/main/java/br/com/vivabem/Main.java
+++ demo/src/main/java/br/com/vivabem/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner teclado = new Scanner(System.in).useDelimiter("\n");
+    private static Scanner teclado = new Scanner(System.in);
 
     public static int exibirMenu() {
         System.out.println("""
@@ -13,7 +13,7 @@ public class Main {
             2 - Listar Moradores
             3 - Adicionar Funcionario
             4 - Adicionar Moradores
-            5 - Buscar Fucnionário
+            5 - Buscar Funcionário
             6 - Sair
             """);
         return teclado.nextInt();
@@ -23,26 +23,26 @@ public class Main {
         System.out.println("Informe o nome do funcionário:");
         var nome = teclado.next();
 
-        System.out.println("Informe o CPF do funcionario:");
+        System.out.println("Informe o CPF do funcionário:");
         var cpf = teclado.next();
 
-        System.out.println("Informe a data de nascimento do funcionario:");
+        System.out.println("Informe a data de nascimento do funcionário:");
         var dt_nascimento = teclado.next();
         
-        System.out.println("Informe salario do funcionario:");
+        System.out.println("Informe salario do funcionário:");
         var salario = teclado.nextDouble();
         
-        System.out.println("Informe o cargo do funcionario:");
+        System.out.println("Informe o cargo do funcionário:");
         var cargo = teclado.next();
 
-        System.out.println("Informe o data de admissao do funcionario:");
+        System.out.println("Informe o data de admissao do funcionário:");
         var dt_admissao = teclado.next();
         
         try {
             Funcionario fun = new Funcionario(nome, cpf, dt_nascimento, cargo, salario, dt_admissao);
+            System.out.println("Funcionario criado com sucesso");
 
             Funcionario.createFuncionario(fun);
-
             System.out.println("Funcionário adicionado com sucesso!");      
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
@@ -52,14 +52,16 @@ public class Main {
 
     public static void main(String[] args) {
         var opcao = exibirMenu();
+        // System.out.println("Opção selecionada: " + opcao);
         while (opcao != 6) {
             try {
                 switch(opcao) {
-                    case 1 -> System.out.println("Não implementado 1");
-                    case 2 -> System.out.println("Não implementado 2");
-                    case 3 -> System.out.println("Não implementado 3");
-                    case 4 -> System.out.println("Não implementado 4");
-                    case 5 -> System.out.println("Não implementado 5");
+                    case 1 ->  System.out.println("Não implementado 1");
+                    case 2 ->  System.out.println("Não implementado 2");
+                    case 3 ->  adicionaFuncionario();
+                    case 4 ->  System.out.println("Não implementado 4");
+                    case 5 ->  System.out.println("Não implementado 5");
+                    default -> System.out.println("Opção inválida");
                 }
             } catch (Exception e) {
                 System.out.println("Erro: " + e.getMessage());
